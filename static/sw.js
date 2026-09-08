84d828b0572eafb7ab95057cf0cf5ccae5a9c54c
Ziedomar696 <Ziedomar696@Gmail.com>
2026-09-08 07:10:15 +0300

    Add files via upload

---
+++ static/sw.js
@@ -0,0 +1,1 @@
+const C="marassi-cctv-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["/static/style.css","/static/manifest.json"]))));self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
